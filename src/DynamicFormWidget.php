c4dd6ce1525595fbf59016b45c0e968c3ad96402
olgert <vaskevich@rozetka.com.ua>
2018-05-17 10:53:26 +0300

    change vendor name

--- src/DynamicFormWidget.php
+++ src/DynamicFormWidget.php
@@ -5,7 +5,7 @@
  * @license   https://github.com/bajadev/yii2-dynamicform/blob/master/LICENSE
  */
 
-namespace kidzen\dynamicform;
+namespace pro100olgert\dynamicform;
 
 use Yii;
 use yii\helpers\Html;
